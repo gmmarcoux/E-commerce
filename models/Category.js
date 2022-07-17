837085bfa29a1d31f2a7bb0e21a05afbd4cd3386
Grace Marcoux <gmmarcoux@me.com>
2022-07-16 22:59:25 -0500

    category.js model

--- models/Category.js
+++ models/Category.js
@@ -2,12 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//Create Category model
 class Category extends Model {}
 
+//define table col & config
 Category.init(
   {
-    // define columns
+    //ID column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+
   {
     sequelize,
     timestamps: false,
