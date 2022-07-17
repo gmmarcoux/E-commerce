21d78844a861760e49a1dc26628188a40c2e330e
Grace Marcoux <gmmarcoux@me.com>
2022-07-16 23:10:05 -0500

    product model

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,49 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //ID
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    //Product_name
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    //Price
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    //stock
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    //Category_ID
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
